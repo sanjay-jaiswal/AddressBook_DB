57bb1ff1a648b7108e7719aeace45a5f0243ca56
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-05-17 11:07:51 +0530

    [Sanju] ADD : Get the size of address book by city or state.

---
+++ UC7_GetTheSizeOfAddressBook.sql
@@ -0,0 +1,45 @@
+--------------Welcome To Address Book DB Using SQL------------
+
+---UC 1 : Create a table Address Book---
+CREATE database AddressBookServiceDB;
+
+---UC 2 : Create a table Address Book---
+
+CREATE TABLE AddressBook
+(
+ID				int identity(1, 1) PRIMARY KEY(ID),
+FirstName		varchar(50),
+LastName		varchar(50),
+Address			varchar(50),
+City			varchar(50),
+State			varchar(50),
+Zip				int,
+PhoneNumber	varchar(15),
+Email			varchar(50),
+)
+
+---UC3 : Insert New contcts to Address Book----
+INSERT INTO AddressBook(FirstName, LastName, Address, City, State, Zip, PhoneNumber, Email)
+VALUES('Sanju', 'Jaiswal', 'Madgaon', 'Margao', 'Goa', '403707', '9988884443', 'sanju@gmail.com'),
+('Akshay', 'Belu', 'Mahabaleshwar', 'Mahabaleshwar City', 'MH', '411032', '9997885554', 'Ak21@gmail.com'),
+('Tushar', 'Khachane', 'Indore', 'Indore City', 'MP', '411055', '9977334443', 'tushark@gmail.com'),
+('Akshay', 'Shinde', 'Undri', 'Pune', 'MH', '411047', '8877884443', 'shinde@gmail.com');
+
+select * from AddressBook;
+
+--UC4 edit existing contact
+UPDATE AddressBook SET PhoneNumber = '7788553300' WHERE FirstName = 'Tushar'
+SELECT * FROM AddressBook;
+
+---UC 5: Delete contact using first name
+DELETE FROM AddressBook WHERE FirstName = 'Tushar';
+SELECT * FROM AddressBook;
+
+---UC 6 : Retrieve person belong to city or state
+SELECT * FROM AddressBook WHERE CITY = 'Margao';
+SELECT * FROM AddressBook WHERE STATE = 'Goa';
+SELECT * FROM AddressBook WHERE CITY = 'Pune' or STATE = 'MH';
+
+---UC 7 : Get the size of address book by city or state.
+SELECT CITY, COUNT(CITY) AS CITY_COUNT FROM AddressBook GROUP BY CITY;
+SELECT STATE, COUNT(STATE) AS STATE_COUNT FROM AddressBook GROUP BY STATE;
